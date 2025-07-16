80b966ae3e27a4fef0d21c7e43ee79c8e08e3ca5
Hasan Hüseyin SALİK <hasansalik0202@gmail.com>
2025-07-16 23:06:41 +0300

    New

--- değişken-uygulama/data.js
+++ değişken-uygulama/data.js
@@ -4,6 +4,6 @@ let cinsiyet = prompt('Cinsiyet :')
 let ders  = prompt('Ders adı :')
 let  not1 = prompt('not1 :')
 let not2 = prompt('not2 :') 
-let ort = (not2 + not1/3)
+let ort = (not1 + not2/3)
 
 console.log(ogrNo + " numaralı " + ad + "(" + cinsiyet+")" + " isimli öğrencinin" + ders + " dersinden aldığı ortalama" + ort)

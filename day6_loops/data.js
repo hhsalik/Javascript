d56f0aa77a4e2afe6c463874bed04193504de840
Hasan Hüseyin SALİK <hasansalik0202@gmail.com>
2025-07-22 16:09:40 +0300

    New

--- day6_loops/data.js
+++ day6_loops/data.js
@@ -21,8 +21,117 @@ for(let a = 5; a >= 0; a--){
  for(let i = 0 ; i <= 3; i++){
     console.log(names[i])
  }
-*/
+
   const namess = ['Hasan', 'Hüseyin', 'Salik', 'Mehmet', 'Asalan', 'Ali']
  for(let a = 0 ; a < namess.length; a++){
     console.log(namess[a])
  }
+
+ const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+const newArr = []
+for(let i = 0; i < countries.length; i++){
+  newArr.push(countries[i].toUpperCase())
+}
+
+console.log(newArr)  // ["FINLAND", "SWEDEN", "NORWAY", "DENMARK", "ICELAND"]
+
+ 
+let i = 0
+while (i <= 5) {
+  console.log(i)
+  i++
+} // 0 1 2 3 4 5
+
+ for(let a = 0 ; a < namess.length; a++){
+    console.log(namess[a])
+ }
+
+let i = 7
+while(i<namess.length){
+    console.log('İsim', namess[i]),
+    i++
+}
+
+do{
+    console.log('isim', namess[i])
+    i++
+}while(i < namess.length)
+
+
+for(let i = 0; i<=5; i++){
+    if(i==3){
+        break
+    }
+    console.log(i)
+}
+
+
+  const namess = ['Hasan', 'Hüseyin', 'Salik', 'Mehmet', 'Asalan', 'Ali']
+
+ for(let name of namess){
+    if(name=='Salik'){
+        console.log(`${name} bulundu.` )
+        break
+    }
+    console.log('Ad', name)
+ }
+*/
+
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+/*
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+for(let i=0; i <=10; i++){
+    console.log(i)
+}
+
+let i = 0
+while(i<11){
+    console.log(i)
+    i++
+}
+
+for(let i = 10; i>-1; i--){
+    console.log(i)
+}
+
+let i = 10
+while(i>-1){
+    console.log(i)
+    i--
+}
+
+let n = 5
+for(let i = 0; i<=n; i++){
+    console.log(i)
+}
+
+for(i=1; i<=10; i++){
+    console.log("#".repeat(i))
+}
+*/
+for(let i = 0; i < 11; i++) {
+   console.log(`${i} x ${i} = ${i*i}`);
+}
